9e5fe964cbe5b0dedad768228648e3dbd2c29604
MUHAMMAD_AJMAL <muhammadajmalvn@gmail.com>
2022-12-05 19:53:01 +0530

    otp token delete

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const state = {
   db: null,
 };
 module.exports.connect = function (done) {
-  const url = "mongodb://localhost:27017";
-  const dbname = "goalcart";
+  const url = process.env.URL;
+  const dbname = "GoalCart";
 
   mongoClient.connect(url, (err, data) => {
     if (err) return done(err);
